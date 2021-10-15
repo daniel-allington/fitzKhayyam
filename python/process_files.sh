cc41ccb65abee9d0488162b44cd62ffd258828c1
Daniel Allington <danielallington76@gmail.com>
2021-10-15 18:53:57 +0100

    shell script to process all five editions to a markdown collection

---
+++ python/process_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python process_file.py ../../source/1859-ed.txt 1859
+python process_file.py ../../source/1868-ed.txt 1868
+python process_file.py ../../source/1872-ed.txt 1872
+python process_file.py ../../source/1879-ed.txt 1879
+python process_file.py ../../source/1889-ed.txt 1889
