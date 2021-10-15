5201e8526f8e71934eac7c23ea8036a371c8abf1
Daniel Allington <danielallington76@gmail.com>
2021-10-15 14:59:39 +0100

    Read lines of file into list, ditch any without alphabetic characters, identify those which are roman numerals (and therefore stanza numbers)

--- python/process_file.py
+++ python/process_file.py
@@ -1,8 +1,16 @@
 import sys
+import re
+import numpy as np
 
-def process_file(a, b):
-    print(a, b)
+alpha = re.compile('[A-Za-z]')
+nonroman = re.compile('[^IVXLC]')
+
+def process_file(fp, version):
+    l = open(fp, 'r').readlines()
+    l = [s.strip() for s in l]
+    l = [s for s in l if len(alpha.findall(s)) > 0]
+    stanza_numbers = np.where([len(nonroman.findall(s.strip('.')))==0 for s in l])
+    print(stanza_numbers[0])
 
 if __name__ == '__main__':
-    process_file(sys.argv[1], sys.argv[2])
-    
+    process_file(fp = sys.argv[1], version = sys.argv[2])
