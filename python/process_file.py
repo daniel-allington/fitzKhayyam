7f7e393f435f6efe9db1b9cc99388eea87768471
Daniel Allington <danielallington76@gmail.com>
2021-12-03 22:58:23 +0000

    Verses of stanza in front matter as separate variables, instead of in body as paragraphs

--- python/process_file.py
+++ python/process_file.py
@@ -12,8 +12,12 @@ dest_folder = os.path.join('..', '_stanzas')
 
 front_matter_template = '''---
 layout: stanza
-edition: {ed}
-stanza: {st}
+edition: {}
+stanza: {}
+v1: {}
+v2: {}
+v3: {}
+v4: {}
 ---
 '''
 
@@ -27,14 +31,17 @@ def process_file(fp, edition):
     stanza_number_lines = np.where([len(rx_nonroman.findall(s.strip('.')))==0 for s in l])[0]
     stanzas = [l[i+1:i+5] for i in stanza_number_lines]
     for n, stanza in enumerate(stanzas):
+        stanza = [rx_digits.sub('', v) for v in stanza]
         with open(
                 os.path.join(
                     dest_folder, 'ed-{ed}-stanza-{st}.md'.format(
                         ed = edition, st = n + 1)), 'w') as f:
             front_matter = front_matter_template.format(
-                ed = edition, st = n + 1)
-            body = rx_digits.sub('', '\n\n'.join(stanza))
-            f.write('\n'.join([front_matter, body]))
+                edition, 
+                n + 1,
+                *stanza
+                )
+            f.write(front_matter)
 
 if __name__ == '__main__':
     process_file(fp = sys.argv[1], edition = sys.argv[2])
